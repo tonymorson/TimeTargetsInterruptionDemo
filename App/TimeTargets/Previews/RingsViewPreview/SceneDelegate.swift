3ec476f18c35b69c3f6246e816a40973d5353c5a
Anthony Morson <tonymorson@gmail.com>
2021-09-13 14:11:48 +0300

    demonstrate how to use individual portrait and landscape modes in RingViewPreview app

--- App/TimeTargets/Previews/RingsViewPreview/SceneDelegate.swift
+++ App/TimeTargets/Previews/RingsViewPreview/SceneDelegate.swift
@@ -2,8 +2,44 @@ import RingsView
 import UIKit
 
 final class RingsViewController: UIViewController {
+  var savedPortraitArrangement: RingsLayout!
+  var savedLandscapeArrangement: RingsLayout!
+
+  var state = RingsViewState() {
+    didSet {
+      if view.isPortrait {
+        savedPortraitArrangement = state.arrangement
+      } else {
+        savedLandscapeArrangement = state.arrangement
+      }
+
+      (view as! RingsView).state = state
+    }
+  }
+
+  var environment = RingsViewEnvironment()
+
   override func loadView() {
-    view = RingsView()
+    view = RingsView { _, ringsAction in
+      ringsViewReducer(state: &self.state, action: ringsAction, environment: self.environment)
+    }
+
+    savedPortraitArrangement = (view as! RingsView).state.arrangement
+    savedLandscapeArrangement = (view as! RingsView).state.arrangement
+
+    savedLandscapeArrangement.concentricity = -1.0
+  }
+
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransition(to: size, with: coordinator)
+
+    coordinator.animate(alongsideTransition: { _ in
+      if size.isPortrait {
+        (self.view as! RingsView).state.arrangement = self.savedPortraitArrangement
+      } else {
+        (self.view as! RingsView).state.arrangement = self.savedLandscapeArrangement
+      }
+    })
   }
 }
 
