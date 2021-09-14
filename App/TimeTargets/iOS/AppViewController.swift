2bd0400000ed60a3483c8d404da5372ff10bef3c
Anthony Morson <tonymorson@gmail.com>
2021-09-14 18:43:22 +0300

    add popup bottom menu button and use reactive constraints to get data mode working

--- App/TimeTargets/iOS/AppViewController.swift
+++ App/TimeTargets/iOS/AppViewController.swift
@@ -116,13 +116,35 @@ class AppViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let toolbar = makeToolbar(imageName: store.dataButtonIconImageName)
+    let toolbar = makeToolbar(imageName: store.dataButtonIconImageName, parentView: view)
+      .moveTo(view) { toolbar, parentView in
+        toolbar.topAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.topAnchor)
+        toolbar.leadingAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.leadingAnchor)
+        toolbar.trailingAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.trailingAnchor)
+        toolbar.heightAnchor.constraint(equalToConstant: 44)
+      }
 
-    toolbar.moveTo(view) { toolbar, parentView in
-      toolbar.topAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.topAnchor)
-      toolbar.leadingAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.leadingAnchor)
-      toolbar.trailingAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.trailingAnchor)
-      toolbar.heightAnchor.constraint(equalToConstant: 44)
+    let bottomMenu = makeMenuButton()
+      .moveTo(view) { bottomMenu, parentView in
+        bottomMenu.centerXAnchor.constraint(equalTo: parentView.centerXAnchor)
+        bottomMenu.bottomAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.bottomAnchor)
+          .reactive(store.appState.map { $0.isShowingData ? 88 : 0 }.eraseToAnyPublisher(),
+                    initialValue: store.appState.value.isShowingData ? 88 : 0)
+      }
+
+    let tabBar = UITabBar()
+    tabBar.items = [
+      UITabBarItem(title: "Today", image: UIImage(systemName: "star.fill"), tag: 0),
+      UITabBarItem(title: "Tasks", image: UIImage(systemName: "list.dash"), tag: 1),
+      UITabBarItem(title: "Charts", image: UIImage(systemName: "chart.pie.fill"), tag: 2),
+    ]
+
+    tabBar.moveTo(view) { tabBar, _ in
+      tabBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 200)
+        .reactive(store.appState.map { $0.isShowingData ? 0 : 200 }.eraseToAnyPublisher(),
+                  initialValue: store.appState.value.isShowingData ? 0 : 200)
+      tabBar.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+      tabBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
     }
 
     store.send(view.isPortrait
@@ -136,7 +158,12 @@ class AppViewController: UIViewController {
         ringsView.leadingAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.leadingAnchor)
         ringsView.trailingAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.trailingAnchor)
         ringsView.topAnchor.constraint(equalTo: toolbar.bottomAnchor)
-        ringsView.bottomAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.bottomAnchor)
+        ringsView.bottomAnchor.constraint(equalTo: bottomMenu.safeAreaLayoutGuide.topAnchor, constant: 0)
+          .reactive(store.appState.map(\.isShowingData).removeDuplicates().map { $0 ? nil : 0 }.eraseToAnyPublisher(),
+                    initialValue: store.appState.value.isShowingData ? nil : 0)
+        ringsView.heightAnchor.constraint(equalToConstant: view.bounds.height - 144)
+          .reactive(store.appState.map(\.isShowingData).removeDuplicates().map { $0 ? 150 : nil }.eraseToAnyPublisher(),
+                    initialValue: store.appState.value.isShowingData ? 150 : nil)
       }
 
     ringsView.output
@@ -187,13 +214,22 @@ class AppViewController: UIViewController {
   }
 }
 
-private func makeToolbar(imageName: AnyPublisher<String, Never>) -> UIStackView {
+private func makeToolbar(imageName: AnyPublisher<String, Never>, parentView: UIView) -> UIStackView {
   let settingsButton = Button(imageSystemName: "gear") {
     store.send(.showSettingsEditorButtonTapped)
   }
 
   let dataButton = Button(imageSystemName: imageName) {
-    store.send(.showDataButtonTapped)
+    parentView.setNeedsLayout()
+
+    UIView.animate(withDuration: 0.35,
+                   delay: 0.0,
+                   usingSpringWithDamping: 0.8,
+                   initialSpringVelocity: 0.3,
+                   options: [.allowUserInteraction]) {
+      store.send(.showDataButtonTapped)
+      parentView.layoutIfNeeded()
+    }
   }
 
   dataButton.transform = CGAffineTransform(rotationAngle: 90 * .pi / 180)
@@ -206,4 +242,89 @@ private func makeToolbar(imageName: AnyPublisher<String, Never>) -> UIStackView 
   }
 
   return toolbar
+}
+
+func makeMenuButton() -> UIButton {
+  var menuItems: [UIAction] {
+    [
+      UIAction(title: "Start Break", image: UIImage(systemName: "arrow.right"), handler: { _ in
+//        viewStore.send(.rings(.ringsTapped(.period)))
+      }),
+      UIAction(title: "Skip Break", image: UIImage(systemName: "arrow.right.to.line"), handler: { _ in
+//        viewStore.send(.rings(.ringsTapped(.period)))
+      }),
+    ].reversed()
+  }
+
+  var demoMenu: UIMenu {
+    UIMenu(title: "", image: nil, identifier: nil, options: [], children: menuItems)
+  }
+
+  var title = AttributedString("Ready for a break?")
+  title.font = UIFont.systemFont(ofSize: 18, weight: .light).rounded()
+
+  var attrString = AttributedString("You have worked 29 minutes taking 3 breaks so far.")
+  attrString.foregroundColor = .label
+
+  var configuration = UIButton.Configuration.gray() // 1
+  configuration.cornerStyle = .dynamic // 2
+  configuration.baseForegroundColor = UIColor.systemRed
+  configuration.baseBackgroundColor = .clear
+  configuration.buttonSize = .small
+  //    configuration.title = "Next work period: 3.55pm"
+  configuration.attributedTitle = title
+  configuration.attributedSubtitle = attrString
+  configuration.titlePadding = 4
+  configuration.titleAlignment = .center
+
+  let button = UIButton(configuration: configuration, primaryAction: nil)
+  button.tintColor = .label
+  button.menu = demoMenu
+  button.showsMenuAsPrimaryAction = true
+
+  return button
+}
+
+extension UIFont {
+  func rounded() -> UIFont {
+    guard let descriptor = fontDescriptor.withDesign(.rounded) else {
+      return self
+    }
+
+    return UIFont(descriptor: descriptor, size: pointSize)
+  }
+}
+
+final class ReactiveLayoutConstraint: NSLayoutConstraint {
+  var cancellables: Set<AnyCancellable> = []
+
+  override init() {
+    super.init()
+  }
+
+  convenience init(constraint: NSLayoutConstraint, constant: AnyPublisher<CGFloat?, Never>) {
+    self.init(item: constraint.firstItem as Any,
+              attribute: constraint.firstAttribute,
+              relatedBy: constraint.relation,
+              toItem: constraint.secondItem,
+              attribute: constraint.secondAttribute,
+              multiplier: constraint.multiplier,
+              constant: constraint.constant)
+
+    constant.sink { value in
+      if let value = value {
+        self.constant = value
+        self.isActive = true
+      } else {
+        self.isActive = false
+      }
+    }
+    .store(in: &cancellables)
+  }
+}
+
+extension NSLayoutConstraint {
+  func reactive(_ publisher: AnyPublisher<CGFloat?, Never>, initialValue _: CGFloat?) -> ReactiveLayoutConstraint {
+    ReactiveLayoutConstraint(constraint: self, constant: publisher)
+  }
 }
