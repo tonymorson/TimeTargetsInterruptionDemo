260c9061421ad5a5d17d97c72564772634f7918b
Anthony Morson <tonymorson@gmail.com>
2021-09-15 22:39:20 +0300

    make BarButtonItem type compatible with older iOS versions

--- Sources/SwiftUIKit/BarButtonItem.swift
+++ Sources/SwiftUIKit/BarButtonItem.swift
@@ -1,6 +1,29 @@
 import Foundation
 import UIKit
 
-public func UIBarButtonItem(_ systemItem: UIBarButtonItem.SystemItem, callback: @escaping () -> Void) -> UIBarButtonItem {
-  UIBarButtonItem(systemItem: systemItem, primaryAction: UIAction(handler: { _ in callback() }))
+public final class BarButtonItem: UIBarButtonItem {
+  var callback: () -> Void
+
+  public convenience init(_ systemItem: UIBarButtonItem.SystemItem, callback: @escaping () -> Void) {
+    self.init(barButtonSystemItem: systemItem, target: nil, action: nil)
+
+    target = self
+    action = #selector(onTap)
+    self.callback = callback
+  }
+
+  override private init() {
+    callback = {}
+
+    super.init()
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  @objc func onTap(sender _: BarButtonItem) {
+    callback()
+  }
 }
