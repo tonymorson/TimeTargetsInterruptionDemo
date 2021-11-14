e7448a962cde213767c222092d1ce123fa3a2172
Anthony Morson <tonymorson@gmail.com>
2021-11-14 10:24:26 +0700

    Reformat long function signature in PromptsFeaturePreview

--- App/TimeTargets/PromptsFeaturePreview/Boilerplate.swift
+++ App/TimeTargets/PromptsFeaturePreview/Boilerplate.swift
@@ -16,7 +16,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
-  func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+  func scene(_ scene: UIScene,
+             willConnectTo _: UISceneSession,
+             options _: UIScene.ConnectionOptions)
+  {
     guard let scene = (scene as? UIWindowScene) else { return }
 
     let window = UIWindow(windowScene: scene)
